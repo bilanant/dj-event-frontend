d628595efd1ef33d2172cb820c999a86a1503d90
bilanant <bilanant@yahoo.com>
2022-06-28 18:53:03 +0530

    Data fetching

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,31 @@
-import Layout from '@/components/Layout'
+import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem'
+import { API_URL } from '@/config/index';
 
-const Events = () => {
+const Events = ({ events }) => {
   return (
-    <Layout>Events</Layout>
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+    </Layout>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: {
+      events,
+      revalidate: 1,
+    }
+  }
 }
 
 export default Events
